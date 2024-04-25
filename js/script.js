1d497ba178f9c539e5c7b75976a567f68db6a25a
psytd11 <psytd11@nottingham.ac.uk>
2024-04-25 12:31:46 +0100

    2.0.1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import {createClient} from 'https://esm.sh/@supabase/supabase-js@2'
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Supabase initialization (replace with your project details)
 const supabaseUrl = 'https://nntrmdwbrsukpbcdledw.supabase.co';
